fed62088caef27db03ac904e09e206e8dc6c0f3c
Vika Smet <vika2001-04@mail.ru>
2024-12-13 19:50:23 +0300

    task#5

--- tasks/ContentView.swift
+++ tasks/ContentView.swift
@@ -8,74 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isAnimating = false
+    let height = UIScreen.main.bounds.height * 0.25
+    @State var position = CGPoint(
+        x: UIScreen.main.bounds.midX,
+        y: UIScreen.main.bounds.midY
+    )
+    let colors: [Color] = [Color.white, .pink, .yellow, .black]
     var body: some View {
-        Button {
-            if !isAnimating {
-                withAnimation(.interpolatingSpring(stiffness: 170, damping: 15)) {
-                    isAnimating = true
-                }
-                completion: {
-                    isAnimating = false
-                }
-            }
-        } label: {
-            GeometryReader { proxy in
-                let width = proxy.size.width / 2
-                let systemName = "play.fill"
-                
-                HStack(alignment: .center, spacing: 0) {
-                    Image(systemName: systemName)
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: isAnimating ?  width: .zero)
-                        .opacity(isAnimating ? 1 : .zero)
-                        .foregroundColor(Color.black)
-                    
-                    Image(systemName: systemName)
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: width)
-                        .foregroundColor(Color.black)
-                        
-                    Image(systemName: systemName)
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: isAnimating ? 0.5 : width)
-                        .opacity(isAnimating ? .zero : 1)
-                        .foregroundColor(Color.black)
-                }
-                .frame(maxHeight: .infinity, alignment: .center)
-                
-                
-            }.frame(maxWidth: 62)
-        }.buttonStyle(CustomButtonStyle(isAnimating: $isAnimating))
-    }
-}
-struct CustomButtonStyle: ButtonStyle {
-    @Binding var isAnimating: Bool
-    private let animationDuration: CGFloat = 0.22
-    private let backgroundColor: Color = Color.gray.opacity(0.3)
-    private let buttonSize: CGFloat = 80
-    
-    func makeBody(configuration: Configuration) -> some View {
         
-        configuration.label
-            .scaleEffect(isAnimating ? 0.6 : 1)
-            .background{
-                Circle()
-                    .frame(maxWidth: buttonSize, maxHeight: buttonSize)
-                    .foregroundStyle(backgroundColor)
-                    .frame(maxHeight: .infinity, alignment: .center)
-                    .scaleEffect(isAnimating ? 1.2 : 1.4)
-                    .opacity(isAnimating ? 1 : 0)
-                    .animation(.easeInOut(duration: animationDuration), value: isAnimating)
+        ZStack {
+            
+            VStack(spacing: 0) {
+                ForEach(colors, id: \.self ) { color in
+                    Rectangle()
+                        .fill(color)
+                        .frame(height: height)
+                }
             }
+            
+            Color.white
+                .blendMode(.difference)
+                .overlay {
+                    Rectangle()
+                        .overlay(.white.blendMode(.overlay))
+                        .overlay(.black.blendMode(.overlay))
+                }
+                .blendMode(.saturation)
+                .frame(width: 100, height: 100)
+                .cornerRadius(10)
+                .position(x: position.x, y: position.y)
+                .gesture(
+                    DragGesture().onChanged { position = $0.location }
+                )
+        }
     }
 }
+
 #Preview {
     ContentView()
 }
